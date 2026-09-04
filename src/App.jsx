02dace3294d03bde35cf22f6ae7b8ad3ac47733d
Corentin Delvaque <corentin.delvaque@icloud.com>
2026-09-04 11:40:05 +0200

    feat: create Profil component and integrate it into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+// App.jsx
+import Profil from "./components/Profil";
+
 function App() {
   return (
-    <div>
-      <h1 className="important">Hello World</h1>
-      <h2>Hello</h2>
-    </div>
+    <>
+      <Profil />
+      <Profil />
+      <Profil />
+    </>
   );
 }
 
