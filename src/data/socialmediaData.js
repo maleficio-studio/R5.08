0ed68df08cf0772698c8cc7de7eed177483d9b70
Corentin Delvaque <corentin.delvaque@icloud.com>
2026-09-10 08:19:01 +0200

    feat: initialize social media platform data collection

---
+++ src/data/socialmediaData.js
@@ -0,0 +1,38 @@
+const socialmediaData = [
+  {
+    id: 1,
+    name: "Facebook",
+    image:
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png",
+    username: "@nathanf",
+    followers: "1987",
+    followerpreview: "12 today",
+  },
+  {
+    id: 2,
+    name: "Twitter",
+    image:
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png",
+    username: "@nathanf",
+    followers: "1044",
+    followerpreview: "99 today",
+  },
+  {
+    id: 3,
+    name: "Instagram",
+    image:
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png",
+    username: "@realnathanf",
+    followers: "11k",
+    followerpreview: "1099 today",
+  },
+  {
+    id: 4,
+    name: "Youtube",
+    image:
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png",
+    username: "Nathan F",
+    followers: "8239",
+    followerpreview: "144 today",
+  },
+];
